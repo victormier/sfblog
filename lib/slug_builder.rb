2d5255e3cdef05fb6d95819c45667b266f33dc83
Victor Mier <victormier@gmail.com>
2014-04-13 21:20:41 -0700

    Add find for show method to slug builder

--- lib/slug_builder.rb
+++ lib/slug_builder.rb
@@ -7,16 +7,6 @@ module SlugBuilder
     self.slug_options = {}
   end
 
-  module ClassMethods
-    def build_slug(options = {})
-      self.slug_options = options
-    end
-  end
-
-  def to_param
-    self.slug
-  end
-
   def set_slug
     column = slug_options[:column_name]
     raise self.class.name unless column
@@ -28,5 +18,21 @@ module SlugBuilder
     slug = "#{slug}-#{count}" if count > 0
 
     self.slug = slug if self.slug.blank?
+  end
+
+  # Class methods
+  module ClassMethods
+    def build_slug(options = {})
+      self.slug_options = options
+    end
+
+    def find_for_show(value)
+      self.find_by_slug(value)
+    end
+  end
+
+  # Instance methods
+  def to_param
+    self.slug
   end
 end
