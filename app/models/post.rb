1cecdf92f80bee22a25d258f85df1b7acf816533
Victor Mier <victormier@gmail.com>
2014-04-14 23:45:28 -0700

    Add some scopes and more to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
   include SlugBuilder
-  attr_accessible :title, :content, :published, :date
+  attr_accessible :title, :content, :cover_text, :published, :date
 
   build_slug column_name: :title
 
   validates_presence_of :title
+
+  scope :published, where(published: true)
+  scope :newest_first, order(arel_table[:date].desc)
+  scope :before_now, where(arel_table[:date].lt(DateTime.now))
 end
