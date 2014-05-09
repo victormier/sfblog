3ddd3e68544015f4994a4f78ef91d56d5b9257ea
Victor Mier <victormier@gmail.com>
2014-05-08 20:59:37 -0600

    Add carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV['S3_SECRET'],
       :region                 => 'eu-west-1'
     }
-    config.fog_directory  = 'BUCKET-NAME'
-    config.asset_host     = 'http://s3-eu-west-1.amazonaws.com/BUCKET-NAME'
+    config.fog_directory  = 'sf-blog'
+    config.asset_host     = 'http://s3-eu-west-1.amazonaws.com/sf-blog'
     config.fog_public     = true
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
   end
